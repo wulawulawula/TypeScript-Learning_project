2c1256662d3d5f6f066885f8d070f59a41dce2c9
oulaoulaoula <m17376757163@163.com>
2026-05-08 14:51:21 +0800

    commit -s -m '类的定义'

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
-import { hi } from "./hello"
-function sum(a: number, b: number): number {
-    return a * b
+// 使用class来定义一个类
+/*
+对象中主要包含两部分
+1.属性
+2.方法
+*/
+class Person {
+    /*
+    直接定义的属性是实例属性，需要通过对象的实例来进行访问
+    per.name
+
+    使用static修饰的是静态属性（类属性）,可以直接通过类来访问
+
+    Person.age
+    */
+    // 定义实例属性
+    // readonly修饰的属性是只读属性，不能修改
+    // readonly name: string = "张三";
+    name: string = "张三";
+    // 在实例前使用static关键字修饰可以定义类属性（静态属性）
+    age: number = 18
+
+    // 定义实例方法
+    sayHi() {
+        console.log("hi")
+    }
+    // 定义类（静态）方法
+    static sayHello() {
+        console.log("hello")
+    }
 }
 
-const obj = {
-    name:'张三',
-    age:33
-}
-console.log(obj)
-obj.age = 18
-console.log(obj)
-console.log(sum(100, 200))
-console.log(hi)
-console.log(Promise)
+const per = new Person()
+per.name = "李四"
+console.log(per.name, per.age)
+per.sayHi()
+Person.sayHello()
---
+++ src/part1/globals.d.ts
@@ -0,0 +1,14 @@
+// 支持 CSS
+declare module '*.css';
+// 支持 SCSS/SASS
+declare module '*.scss';
+declare module '*.sass';
+// 支持 LESS
+declare module '*.less';
+// 支持图片
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.webp';

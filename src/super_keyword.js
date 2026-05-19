bd9407404dbcd80c4e1f521734111e313e8714fa
oulaoulaoula <m17376757163@163.com>
2026-05-19 10:27:00 +0800

    commit -s -m 'super关键字'

--- src/super_keyword.js
+++ src/super_keyword.js
@@ -5,5 +5,23 @@
             this.age = age;
             this.name = name;
         }
+        sayHello() {
+            console.log('这是Animal类中提供的方法');
+        }
     }
+    class Dog extends Animal {
+        constructor(name, age, isMale) {
+            // 如果子类中写了构造函数，则必须在构造函数中调用super()方法,并进行赋值
+            super(name, age);
+            this.isMale = isMale;
+        }
+        sayHello() {
+            // 在类的方法中，super代表当前类的父类
+            super.sayHello(); // super关键字可以用来调用父类的方法
+            console.log('这是Dog类中提供的方法');
+        }
+    }
+    const dog = new Dog('小黑', 4, false);
+    dog.sayHello();
+    console.log(dog);
 })();
